a0f9a48f85c52841951afdf5a43f87c88c535412
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-03-01 20:26:22 +0500

    new Commit

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 "use strict";
+let elForm = document.querySelector(".todo_form");
+let elInput = document.querySelector(".todo_input");
+let elList = document.querySelector(".list");
+const todos = [];
+elForm === null || elForm === void 0 ? void 0 : elForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = {
+        id: todos.length ? todos[todos.length - 1].id + 1 : 1,
+        value: elInput.value,
+        isDone: false
+    };
+    todos.push(data);
+    renderTodos(todos, elList);
+    e.target.reset();
+});
+function renderTodos(arr, list) {
+    if (list)
+        list.innerHTML = "";
+    arr.forEach((item, index) => {
+        let elItem = document.createElement("li");
+        elItem.className = "flex items-center justify-between p-2 rounded-md bg-white";
+        elItem.innerHTML = `
+             <div class="flex items-center gap-1">
+                <span>${index + 1}</span>
+                <strong>${item.value}</strong>
+             </div>
+             <button onclick="handleDelete(${index})" class="bg-red-500 p-2 text-white rounded-md">Delete</button>
+          `;
+        list === null || list === void 0 ? void 0 : list.appendChild(elItem);
+    });
+}
+renderTodos(todos, elList);
+function handleDelete(id) {
+    todos.splice(id, 1);
+    renderTodos(todos, elList);
+}
---
+++ main.ts
@@ -0,0 +1,46 @@
+let elForm:Element | null = document.querySelector(".todo_form")
+let elInput:Element | null = document.querySelector(".todo_input")
+let elList:Element | null = document.querySelector(".list")
+
+type TodoType = {
+    id:number,
+    value:string,
+    isDone:boolean
+}
+
+const todos:TodoType[] = []
+
+elForm?.addEventListener("submit", (e:Event):void=> {
+    e.preventDefault()
+    const data:TodoType = {
+        id:todos.length ? todos[todos.length -1].id + 1 : 1,
+        value:(elInput as HTMLInputElement).value,
+        isDone:false
+    }
+    todos.push(data)
+    renderTodos(todos, elList);
+    (e.target as HTMLFormElement).reset()
+})
+function renderTodos(arr:TodoType[], list:Element | null):void{
+       if(list) list.innerHTML = ""
+       arr.forEach((item:TodoType, index:number)=> {
+          let elItem:Element = document.createElement("li")
+          elItem.className = "flex items-center justify-between p-2 rounded-md bg-white"
+          elItem.innerHTML = `
+             <div class="flex items-center gap-1">
+                <span>${index + 1}</span>
+                <strong>${item.value}</strong>
+             </div>
+             <button onclick="handleDelete(${index})" class="bg-red-500 p-2 text-white rounded-md">Delete</button>
+          `
+
+          list?.appendChild(elItem)
+       })
+}
+
+renderTodos(todos, elList)
+
+function handleDelete(id:number):void{
+    todos.splice(id, 1)
+    renderTodos(todos, elList)
+}
